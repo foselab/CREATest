2a60c23580a32601705062b3409f768ba9acaf21
nicopellegrinelli <nico.pellenp@gmail.com>
2025-03-14 10:12:00 +0100

    Fix experiments code

--- experiments/run-createst.bat
+++ experiments/run-createst.bat
@@ -1,3 +1,3 @@
 @echo off
 set "sccPath=C:\Users\nico.pellegrinelli\Desktop\Eclipse\itemis_CREATE\scc.bat"
-powershell -Command "& { $env:sccPath='%sccPath%'; Get-ChildItem benchmark\*.ysc | Sort-Object Name | ForEach-Object { java -jar CREATest-0.0.3.jar -s $env:sccPath -y ('benchmark\' + $_.BaseName + '.ysc') -g -e | Tee-Object -FilePath ($_.BaseName + '.txt') } }"
+powershell -Command "& { $env:sccPath='%sccPath%'; Get-ChildItem benchmark\*.ysc | Sort-Object Name | ForEach-Object { java -jar CREATest-0.0.3.jar -s $env:sccPath -y ('benchmark\' + $_.BaseName + '.ysc') -g -e | Tee-Object -FilePath ($_.BaseName + '_exec.txt') } }"
